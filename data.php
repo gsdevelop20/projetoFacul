e320b1f4413dadca77b09d87f6056914ce532c51
Gabriel <gabrielsantosdesousa20@gmail.com>
2023-05-12 12:16:58 -0300

    insert mustache templetes add composer

--- data.php
+++ data.php
@@ -2,4 +2,6 @@
 
 $num1 = $_POST['num1'];
 $num2 = $_POST['num2'];
-echo $num1 + $num2;
+include 'vendor\mustache\mustache\src\Mustache\Engine.php';
+$m = new Mustache_Engine();
+echo $m->render('Hello, {{planet}}!', ['planet' => 'World']); // "Hello, World!"
--- index.php
+++ index.php
@@ -1,23 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bootstrap-4.6.2-dist/css/bootstrap.css">
-    <title>Document</title>
-</head>
-<body>
-    <input id="i1" type="number">
-    <input id="i2" type="number">
-    <a class="btn btn-secondary mt-5" href="#">EXECUTAR</a>
-    <div id="r"></div>
-    
-<?php  
-   
-?>
-<script type="text/javascript" src="node_modules/jquery/dist/jquery.js"></script>
-<script src="bootstrap-4.6.2-dist/js/bootstrap.js"></script>
-<script type="text/javascript" src="src/index.js"></script>
-</body>
-</html>
+
+<?php
+include 'vendor\mustache\mustache\src\Mustache\Autoloader.php';
+
+Mustache_Autoloader::register();
+
+$m = new Mustache_Engine(array(
+    'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/templates'),
+));
+
+
+echo $m->render("head", ['title' => 'meu site']); // title
+echo $m->render("navbar", []); // navbar
+echo $m->render("teste", ['planet' => 'World']); // "Hello, World!"
+
